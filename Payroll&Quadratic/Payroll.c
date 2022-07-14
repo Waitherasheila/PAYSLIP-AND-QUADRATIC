6f19afc4dee7f4b74cdc77992cd753bcdf27c34c
Sheila Waithera <106369293+Waitherasheila@users.noreply.github.com>
2022-07-14 13:35:44 +0300

    Add files via upload

---
+++ Payroll&Quadratic/Payroll.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+float main()
+{
+printf("Gitonga's factory\n");
+printf("Employee number\n");
+printf("KRA Pin\n");
+scanf("%d",&KRA Pin);
+KRA Pin = 30/100 * total income;
+printf("KRA Pin=%d",&KRA Pin);
+printf("Number of hours worked\n");
+scanf("%d",&hours worked);
+if(hours worked = 40hours)
+{
+    printf("1000 ksh");
+}
+else
+    if(hours worked > 40hours)
+{
+    printf("1500");
+}
+normal pay = regular pay rate*40hours;
+   printf("normal pay=%d",&normal pay);
+overtime pay = regular pay rate 1.5 * 40hours;
+   printf("overtime pay=%d",&overtime pay);
+tax paid = 30/100*total income;
+   printf("tax paid=%d", &tax paid);
+net income = total income - (30/100 * total income)
+   printf("net income=%d", &net icome);
+return 0;
+}
---
+++ Payroll&Quadratic/QE.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+float a,b,c;
+main()
+{
+    printf("enter a");
+    scanf("%f",& a\n);
+    printf("enter b");
+    scanf("%f",& b\n);
+    printf("enter c");
+    scanf("%f",& c\n);
+
+    x=(-b+-(b^2-4ac))/2a;
+    printf("x=%f", &x);
+
+    if(b^2-4ac=0)
+    {
+        printf("equation has two equal values\n");
+    }
+    else
+        if(b^2-4ac>0)
+    {
+        printf("equation has two distinct values\n");
+    }
+    else
+        if(b^2-4ac<0)
+    {
+        printf("equation has no roots\n");
+    }
+return 0;
+}
+
